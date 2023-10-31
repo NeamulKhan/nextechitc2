47861fbbff96ed01acb106904221e929dd87d63a
Neamul Khan <neamulkhan@yahoo.com>
2023-10-30 23:48:08 -0400

    update HomePage

--- src/test/java/pageLocator/HomePageLocator.java
+++ src/test/java/pageLocator/HomePageLocator.java
@@ -1,10 +1,14 @@
 package pageLocator;
 
+import org.openqa.selenium.support.PageFactory;
+
 import utility.BaseClass;
 
 public class HomePageLocator extends BaseClass{
 
   public HomePageLocator(){
+	  
+	  PageFactory.initElements( driver,this);
 
     
 
